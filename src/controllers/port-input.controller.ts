42a4b9681952d9b3f7e843daa173b57c1a4be8f6
Eduardo-Rueda-de-Santiago <115037378+Eduardo-Rueda-de-Santiago@users.noreply.github.com>
2026-04-20 02:06:06 +0200

    Added settings file  + controller for audio line

---
+++ src/controllers/port-input.controller.ts
@@ -0,0 +1,2 @@
+export async function handleEspLine(line: string) {}
+export async function handleEspError(exception: Error) {}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
 import createPort from "./utils/port";
+import { ReadlineParser } from "@serialport/parser-readline";
+import {
+  handleEspError,
+  handleEspLine,
+} from "./controllers/port-input.controller";
+
+const port = createPort();
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+
+port.on("open", () => {
+  console.log(`Listening on ${port.conn} at ${port.baudRate} baud...`);
+});
+
+parser.on("data", handleEspLine);
+
+port.on("error", handleEspError);
